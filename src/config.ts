be340d0bd14635b1e9dab29c3298e3433a8bbb0d
huizhiLLL <huizhiLLL@users.noreply.github.com>
2026-06-08 17:22:21 +0800

    feat：giscus

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
   title: "Weekly-Memos",
   author: "huizhiLLL",
-  description: "huizhiLLL 的个人周记、阅读记录与生活备忘",
+  description: "huizhiLLL 的个人周记、生活记录与思想感悟",
   keywords: "huizhiLLL,Weekly-Memos,Weekly,Blog,周记,备忘",
   icon: "https://weekly.huizhi.ink/icon-144.png",
   siteImage: "https://weekly.huizhi.ink/assets/1.svg",
@@ -13,15 +13,15 @@ export const SITE = {
 };
 
 export const GISCUS_CONFIG = {
-  repo: "",
-  repoId: "",
+  repo: "huizhiLLL/Weekly",
+  repoId: "R_kgDOSXmBPA",
   category: "General",
-  categoryId: "",
-  mapping: "title",
+  categoryId: "DIC_kwDOSXmBPM4C-vZ3",
+  mapping: "specific",
   strict: "0",
   reactionsEnabled: "1",
   emitMetadata: "0",
-  inputPosition: "bottom",
+  inputPosition: "top",
   theme: "https://gw.alipayobjects.com/os/k/kr/comment.css",
   lang: "zh-CN",
 };
